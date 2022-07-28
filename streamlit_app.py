5ea568c9fb8962fe386b59e12b7b773e4891648c
MukeshT <7647609+mtsoa@users.noreply.github.com>
2022-07-28 16:41:08 -0500

    Update streamlit_app.py
    emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Blueberry oatmeal & omega 3')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hardboiled free range eggs')
+streamlit.header('👌Breakfast favourites')
+streamlit.text('🌚Blueberry oatmeal & omega 3')
+streamlit.text('🌾Kale, spinach & rocket smoothie')
+streamlit.text('🥚Hardboiled free range eggs')
